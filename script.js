a934ab9db34fc2be0078b06076c597a3b88a3c36
Helen0105 <marinenkoolena21@gmail.com>
2025-10-24 21:54:41 +0300

    hm 1

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log(1);
+
+
+const num = [1, 2, 3];
+num[1] = 10;
+console.log(num);
+
+console.log(2);
+
+const arr = ['Olena', 'Valera', 'Helen']
+arr[3] = 'Anna';
+console.log(arr);
+
+console.log(3);
+
+
+const sum = [1, 2, 3]
+
